91f525cf187d03c9a3876991d7e880a8e734af5b
Majo Paskuvan <majopaskuvan@gmail.com>
2026-02-27 22:47:14 -0300

    first commit

---
+++ holamundo.py
@@ -0,0 +1,6 @@
+print("Hola mundo")
+print ("Bienvenidos a la programación en Python")
+
+
+# Este es un comentario de una sola línea
+
---
+++ tiposdedatos.py
@@ -0,0 +1,57 @@
+# numero entero => integer: int
+
+numero = 123
+print (numero)
+print (type(numero))
+
+# numero decimal => float
+
+decimal = 3.14
+print (decimal)
+print (type(decimal))
+
+# texto => string: str
+
+texto = "Hola mundo"
+print (texto)
+print (type(texto))
+
+# valor booleano => boolean: bool
+
+verdadero = True
+falso = False
+print (verdadero)
+print (falso)
+print (type(verdadero))
+print (type(falso))
+
+# valor nulo => None
+
+nulo = None
+print (nulo)
+print (type(nulo))
+
+# lista => list
+
+mi_lista = [1, 2, 3, "cuatro", "cinco"]
+print (mi_lista)
+print (type(mi_lista))
+
+# tupla => tuple
+
+mi_tupla = (1, 2, 3, "cuatro", "cinco")
+print (mi_tupla)
+print (type(mi_tupla))
+
+# conjunto => set
+
+mi_conjunto = {1, 2, 3, "cuatro", "cinco"}
+print (mi_conjunto)
+print (type(mi_conjunto))
+
+# diccionario => dictionary: dict
+
+mi_diccionario = {"nombre": "Juan", "edad": 30, "ciudad": "Madrid"}
+print (mi_diccionario)
+print (type(mi_diccionario))
+
