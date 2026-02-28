f135466865424afddb518fce86925417d93534fa
Majo Paskuvan <majopaskuvan@gmail.com>
2026-02-27 23:04:56 -0300

    second commit

---
+++ casting.py
@@ -0,0 +1,18 @@
+# Cast => cambiar de un tipo de dato a otro
+# castear (convertir) de un tipo de dato a otro
+
+edad = "25"
+edad = int(edad)  # Convertir la cadena "25" a un número entero
+print(type(edad))
+
+# de int a str
+
+edad_int = 10
+edad_int = str(edad_int)  # Convertir el número entero 10 a una cadena "10"
+print(type(edad_int))
+
+# de str a float
+
+altura_str = "1.75"
+altura_float = float(altura_str)  # Convertir la cadena "1.75"
+print(type(altura_float))
