b7585b07ad0a501fbe1ce8b7f4850be5a4cafb88
Majo Paskuvan <majopaskuvan@gmail.com>
2026-02-28 22:11:59 -0300

    Concatenar

---
+++ concatenar.py
@@ -0,0 +1,6 @@
+nombre = "Majo"
+edad = "20"
+
+texto = f"Mi nombre es {nombre} y tengo {edad} años."
+
+print (texto)
---
+++ variables_vacias.py
@@ -0,0 +1,14 @@
+#string
+
+mi_variable = ""
+mi_variable = str()
+
+#integer
+
+edad = int()
+
+#boolean
+
+edad = bool()
+
+print (edad)
