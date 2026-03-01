0f2f0644f33e6d710fe607d609947a5536c63512
Majo Paskuvan <majopaskuvan@gmail.com>
2026-03-01 17:48:56 -0300

    notes

--- concatenar.py
+++ concatenar.py
@@ -1,6 +1,10 @@
-nombre = "Majo"
-edad = "20"
+nombre = "Javiera"
+edad = "30"
 
 texto = f"Mi nombre es {nombre} y tengo {edad} años."
 
 print (texto)
+
+texto2 = f"{nombre} es el dueña de esta computadora y tiene {edad} años."
+
+print (texto2)
